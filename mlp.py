dc752d11cc3da3bbce40bc547f88b5e8f1b699d9
ArtemisMucaj <artemis.mucaj@gmail.com>
2015-11-11 19:25:12 +0100

    updated mlp

--- mlp.py
+++ mlp.py
@@ -4,6 +4,8 @@ import pickle
 import cv2
 import projection as proj
 
+import matplotlib.pyplot as plt
+
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.optimizers import SGD
@@ -11,17 +13,19 @@ from keras.optimizers import SGD
 with open('classes.pickle', 'rb') as f:
     classes = pickle.load(f)
 
+targets = np.zeros((len(classes),1))
+
 training = np.zeros((len(classes),997))
 nb_examples, data = training.shape
 
-for i in range(0,10):
+for i in range(0,2):
     print("Working on page : "+str(int(classes[i][0]))+", class : "+str(classes[i][1]))
     img = cv2.imread("hpc_low/-"+str(int(classes[i][0]))+".pgm")
     training[:][i] = proj.projection(img)
+    targets[i] = classes[i][1]
 
 x_test = proj.projection(cv2.imread("hpc_low/-"+str(int(classes[25][0]))+".pgm"))
-y_test = classes[25][1]
-
+y_test = targets[25]
 
 model = Sequential()
 # Dense(64) is a fully-connected layer with 64 hidden units.
@@ -32,7 +36,7 @@ model.add(Dropout(0.5))
 model.add(Dense(64, init='uniform'))
 model.add(Activation('tanh'))
 model.add(Dropout(0.5))
-model.add(Dense(2, init='uniform'))
+model.add(Dense(1, init='uniform'))
 model.add(Activation('softmax'))
 
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
