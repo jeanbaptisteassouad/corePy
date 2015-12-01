4f0d3baaae2798703ebc34a873aa7f6b38eb80e0
ArtemisMucaj <artemis.mucaj@gmail.com>
2015-12-01 14:39:30 +0100

    improved tesseract_tirals

--- tesseract_trials.py
+++ tesseract_trials.py
@@ -1,15 +1,16 @@
 import cv2
 # import numpy as np
+import subprocess
 
 from Content import Content
 from TableFactory import TableFactory
 
 from PIL import Image
-from pytesseract import image_to_string
+
 
 def ocr():
     contenu = Content()
-    table = TableFactory("core/table.png",contenu.paris_dichotomie)
+    table = TableFactory("core/table2.png",contenu.paris_dichotomie)
     table.tree_of_content.remove_useless_leaf()
     table.tree_of_content.sort_subtree_by_frame()
     # get leafs
@@ -18,21 +19,18 @@ def ocr():
 
     for elt in leafs:
         subimage = table.image[elt.frame[0][1]:elt.frame[1][1],elt.frame[0][0]:elt.frame[1][0]]
-        print(image_to_string(subimage))
+        cv2.imwrite("tmp.tiff",subimage)
+        # Subprocess
+        subprocess.call(['tesseract', 'tmp.tiff', 'output'])
+        with open('output.txt') as f:
+            text = f.read()
+        print("#####")
+        print(text)
+
         cv2.imshow("image",subimage)
         cv2.waitKey()
 
     pass
 
-def test_ocr():
-    # tesseract trial
-    img = Image.open("core/table.png")
-    string = (image_to_string(img))
-    print(" text : "+string)
-    exit()
-    pass
-
-
-
 if __name__ == '__main__':
-    test_ocr()
+    ocr()
