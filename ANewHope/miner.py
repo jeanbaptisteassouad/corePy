697c8b0b602b2d4ef53119419328a2afa559aa41
Jean-Baptiste Assouad <jean.baptiste@assouad.com>
2016-01-29 00:20:30 +0100

    Don't know the change ...

--- ANewHope/miner.py
+++ ANewHope/miner.py
@@ -1,39 +1,6 @@
 import re
 
 
-def string_to_regex(string):
-	ans = ''
-	last_regex = ''
-	for i in range(0,len(string)):
-		if re.search('[0-9,]',string[i]) != None:
-			if last_regex == '[0-9,]+':
-				pass
-			else:
-				ans += '[0-9,]+'
-				last_regex = '[0-9,]+'
-		elif re.search('[A-Z]',string[i]) != None:
-			if last_regex == '[A-Z]+':
-				pass
-			else:
-				ans += '[A-Z]+'
-				last_regex = '[A-Z]+'
-		elif re.search('[a-z]',string[i]) != None:
-			if last_regex == '[a-z]+':
-				pass
-			else:
-				ans += '[a-z]+'
-				last_regex = '[a-z]+'
-		else:
-			ans += string[i]
-			last_regex = string[i]
-	return ans;
-
-def gen_regex(list_words):
-	ans = []
-	for x in range(0,len(list_words)):
-		ans.append( string_to_regex(list_words[x]) )
-	return ans
-
 
 def miner_lineIsNumeric(line):
 	chars = list(line)
@@ -94,7 +61,7 @@ def main():
 
 
 
-	number_page = 760
+	number_page = 507
 	lines = re.split('\n',pages[number_page])
 
 
@@ -111,7 +78,6 @@ def main():
 	##############
 	# Processing #
 	##############
-
 	#On garde les lignes ou il y au moins la moitier de chiffres
 	pages[number_page] = miner_keepOnlyNumericLine(pages[number_page])
 
@@ -120,34 +86,6 @@ def main():
 
 	print pages[number_page]
 
-	# list_regex = []
-	# for x in range(0,len(lines)):
-	# 	tmp = re.split(' {2,}',lines[x])
-	# 	# print tmp
-	# 	list_regex.append( gen_regex(tmp) )
-	# 	# for y in range(0,len(lines[x])):
-	# 	# 	print lines[x][y], re.search('[0-9]',lines[x][y]) != None, re.search('[A-B]',lines[x][y]) != None, re.search('[a-z]',lines[x][y]) != None
-	# 	# 	pass
-	# 	pass
-
-	# for x in range(0,len(list_regex)):
-	# 	print list_regex[x]
-	# 	pass
-
-	# nb_spaces_per_lines = []
-	# for x in range(0,len(lines)):
-	# 	nb_spaces_per_lines.append( len(re.findall('[^ ] {2,}',lines[x])) )
-
-
-	# for x in range(0,len(nb_spaces_per_lines)-1):
-	# 	if nb_spaces_per_lines[x] != nb_spaces_per_lines[x+1]:
-	# 		nb_spaces_per_lines[x] = 0
-	# 	else:
-	# 		if nb_spaces_per_lines[x] != 0:
-	# 			print x, ":", nb_spaces_per_lines[x]
-
-
-	pass
 
 if __name__ == '__main__':
 	main()
